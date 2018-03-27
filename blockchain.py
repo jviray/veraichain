64e52d378a119f2579cff25e87f43856a152e9f5
Jonathan Viray <jfviray7@gmail.com>
2018-03-27 17:39:41 -0400

    Add Proof of Work algorithm

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 class Blockchain():
 	def __init__(self):
@@ -10,7 +12,6 @@ class Blockchain():
 
 		# Create the genesis block
 		self.new_block(previous_hash=1, proof=100)
-
 
 	def new_block(self, proof, previous_hash=None):
 		"""
@@ -36,7 +37,6 @@ class Blockchain():
 		self.chain.append(block)
 		return block
 
-
 	def new_transaction(self, sender, recipient, amount):
 		"""
 		Creates a new transaction to the list of transactions
@@ -54,7 +54,6 @@ class Blockchain():
 			'amount': amount,
 		})
 
-
 	@staticmethod # !!!
 	def hash(block):
 		"""
@@ -67,7 +66,6 @@ class Blockchain():
 		block_string = json.dumps(block, sort_keys=True).encode()
 		return hashlib.sha256(block_string).hexdigest()
 		
-
 	@property
 	def last_block(self):
 		"""Returns the last block in the chain"""
@@ -91,3 +89,45 @@ class Blockchain():
 		})
 
 		return self.last_block['index'] + 1
+
+	def proof_of_work(self, last_proof):
+		"""
+		Proof of Work Algorithm:
+		 - Finds a number p' such that hash (pp') contains 4 leading 
+		   zeroes
+		 - p is the previous proof, and p' is the new proof
+
+		 Returns <int> proof
+		 """
+
+		 proof = 0
+		 while self.valid_proof(last_proof, proof) is False:
+		 	proof += 1
+
+		 return proof
+
+	@staticmethod
+	def valid_proof(last_proof, proof):
+		"""
+		Validates the Proof of Work by verifying that a has contains
+		4 leading zeroes.
+
+		last_proof: <int> Previous proof
+		proof: <int> Current proof
+		"""
+
+		guess = f'{last_proof}{proof}'.encode()
+		guess_hash = hashlib.sha256(guess).hexdigest()
+		return guess_hash[:4] == '0000'
+
+
+
+
+
+
+
+
+
+
+
+
